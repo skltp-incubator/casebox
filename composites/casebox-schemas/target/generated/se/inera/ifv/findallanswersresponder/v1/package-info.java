2ecd2f4a985ca0f654dcb504fda7c808685af832
hanswiklund <hans.wiklund@kentor.se>
2016-01-15 16:46:05 +0100

    ignore

--- composites/casebox-schemas/target/generated/se/inera/ifv/findallanswersresponder/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:riv:insuranceprocess:healthreporting:FindAllAnswersResponder:1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.inera.ifv.findallanswersresponder.v1;
--- modules/web/target/classes/insert_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO casebox.USERS VALUES ('admin', 1, '', '', 'casebox');
-INSERT INTO casebox.AUTHORITIES VALUES (1, 'ROLE_USER', 'admin');
