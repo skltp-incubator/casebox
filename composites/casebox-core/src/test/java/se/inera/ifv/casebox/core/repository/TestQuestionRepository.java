423db69d2fa42affa48c41750b17a5afd994d653
EMEAAD\gerkstam <goran.erkstam@soprasteria.com>
2020-11-25 09:38:15 +0100

    NTP-1763 - Security updates

--- composites/casebox-core/src/test/java/se/inera/ifv/casebox/core/repository/TestQuestionRepository.java
+++ composites/casebox-core/src/test/java/se/inera/ifv/casebox/core/repository/TestQuestionRepository.java
@@ -30,7 +30,6 @@ import javax.persistence.PersistenceContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
